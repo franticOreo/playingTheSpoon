96571948d50f265e8f70db7a1f25a1ea2c03c9fe
franticOreo <eli.simic.robertson@gmail.com>
2018-08-14 11:30:19 +0000

    starting

---
+++ start.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+R = False
+B = True
+
+color = [R,B]
+num = []
+# bet_size = [x]
+
+x = np.random.rand(1,i)
+
+
+def randomNumber(  ):
+  for i in range(0,36):
+    x = np.random.rand(1,i)
+    print(x)
+  return x
+randomNumber()
